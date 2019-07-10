49b3a1b3d59cf54c4ec3026388e61209e8b71501
Andrew Hess <andrew.hess@affirm.com>
2019-07-10 10:23:44 -0700

    adding web pages

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_assets import Bundle, Environment
+
 app = Flask(__name__)
+assets = Environment(app)
+
+# add JS contents
+js = Bundle('main.js')
+assets.register('js_all', js)
+
+# add less CSS
+css = Bundle('main.less', filters='less', output='main.css')
+assets.register('css_all', css)
+
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def product_page():
+  return render_template('product.html')
 
+
+@app.route('/checkout')
+def checkout_page():
+  return render_template('checkout.html')
